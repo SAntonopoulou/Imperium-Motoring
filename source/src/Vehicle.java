3e58ad9e28abdb97949d0736aa528246d1696d9c
Sl1PPYNATOR <marioslambropoulos2003@gmail.com>
2024-11-25 17:46:30 +0200

    finished variables/enums and constructor on vehicle class

--- source/src/Vehicle.java
+++ source/src/Vehicle.java
@@ -22,4 +22,24 @@ public class Vehicle {
     private String interiorColour;
     //Need to add notes variable when changed to longtext
 
+    public Vehicle(int ID, String make, String model, String variant, LocalDate registration, FuelType fuelType, Category category, Drivetrain drivetrain, int price, int mileage, int engineSize, int enginePower, EnginePosition enginePosition, EngineType engineType, Transmission transmission, String colour, String interiorColour) {
+        this.ID = ID;
+        this.make = make;
+        this.model = model;
+        this.variant = variant;
+        this.registration = registration;
+        this.fuelType = fuelType;
+        this.category = category;
+        this.drivetrain = drivetrain;
+        this.price = price;
+        this.mileage = mileage;
+        this.engineSize = engineSize;
+        this.enginePower = enginePower;
+        this.enginePosition = enginePosition;
+        this.engineType = engineType;
+        this.transmission = transmission;
+        this.colour = colour;
+        this.interiorColour = interiorColour;
+    }
 }
+
